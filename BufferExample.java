2dbf31d898d4aa8a116c309db727ea6e5b15b56c
Sreedharsan24 <162305938+Sreedharsan24@users.noreply.github.com>
2024-10-17 06:52:50 +0530

    Add files via upload

---
+++ BufferExample.java
@@ -0,0 +1,8 @@
+import java.lang.StringBuffer;
+public class BufferExample {
+    public static void main(String[] args) {
+        String name;
+        StringBuffer sb = new StringBuffer("SreeDharsan");
+        System.out.println(sb);
+    }
+}
